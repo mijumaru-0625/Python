163ebd547400770cfaa19adba8ae2dc165fe4506
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-17 08:21:58 +0900

    🐜3 p.58 課題 1.4.9

--- play_with_c.py
+++ play_with_c.py
@@ -1,13 +1,3 @@
-def solve1(a, b, c):
-    """ ax + b = c を解く関数 """
-    return (c - b) / a
-
-# 10x + 5 = 30 を解く
-print(solve1(10, 5, 30))
-
-# (10 + 5i)x + 5 = 20 を解く
-print(solve1(10 + 5j, 5, 20))
-
 # plotting.py を http://resources.codingthematrix.com/ からダウンロード
 from plotting import plot 
 
@@ -15,5 +5,12 @@ S = { 2 + 2j, 3 + 2j,
       1.75 + 1j, 2 + 1j, 2.25 + 1j, 2.5 + 1j, 2.75 + 1j, 3 + 1j, 3.25 + 1j}
 plot(S, 4) # ブラウザ（Edge）が開いていないと正しく動作しない
 
-plot({1 + 2j + z for z in S}, 4)
-plot({-2 - 2j + z for z in S}, 4)
+plot({z / 2 for z in S}, 4)
+#plot({-1 * z for z in S}, 4)
+#plot({1j * z for z in S}, 4)
+
+# 課題 1.4.8
+plot({z * 1j * 0.5 for z in S}, 4)
+
+# 課題 1.4.9
+plot({z * 1j * 0.5 + 2 - 1j for z in S}, 4)
