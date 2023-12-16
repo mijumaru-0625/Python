fef802c031d450c433d8a3750cb2e5f0e9cf626c
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-17 08:16:09 +0900

    🐜2 p.56 平行移動

--- play_with_c.py
+++ play_with_c.py
@@ -13,4 +13,7 @@ from plotting import plot
 
 S = { 2 + 2j, 3 + 2j, 
       1.75 + 1j, 2 + 1j, 2.25 + 1j, 2.5 + 1j, 2.75 + 1j, 3 + 1j, 3.25 + 1j}
-plot(S, 4)
+plot(S, 4) # ブラウザ（Edge）が開いていないと正しく動作しない
+
+plot({1 + 2j + z for z in S}, 4)
+plot({-2 - 2j + z for z in S}, 4)
