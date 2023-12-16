a2b8989044a3ea61ace8f22ec719d0a6bf56f713
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-16 14:03:36 +0900

    🐜1 p.78 メッセージボックスを表示する

---
+++ python_excel.py
@@ -0,0 +1,16 @@
+"""
+今すぐ使える
+かんたん biz
+Python
+  x
+Excel 自動処理
+ビジネス活用
+大全
+"""
+
+import tkinter
+from tkinter import messagebox
+
+root = tkinter.Tk()
+root.withdraw()
+messagebox.showinfo("お知らせ", "作業を開始してください")
