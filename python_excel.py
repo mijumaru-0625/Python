031c11b11feeaac8b2c809f6cd094724ac1cc456
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-16 16:36:13 +0900

    🐜3 p.72 独自のクラスを作成しよう
    クラス変数とインスタンス変数の使い方が微妙、間違っていてると思う。実害はないが。
    
    ↓ こっちの方が正しい
    https://uxmilk.jp/41600

--- python_excel.py
+++ python_excel.py
@@ -11,9 +11,48 @@ Excel 自動処理
 import tkinter
 from tkinter import messagebox
 
-#def show_messagebox():
-#    """ メッセージボックスを表示する """
+def show_messagebox():
+    """ メッセージボックスを表示する """
+    #root = tkinter.Tk()    # この行不要だった?
+    #root.withdraw()        # この行不要だった?
+    messagebox.showinfo("お知らせ", "作業を開始してください")
 
-#root = tkinter.Tk()
-#root.withdraw()
-messagebox.showinfo("お知らせ", "作業を開始してください")
+#show_messagebox()
+    
+class Player:
+    score = 0  # これはクラス変数ではないのか? クラス.変数名でアクセスする必要あり
+
+p1 = Player()
+p2 = Player()
+print("値設定前、インスタンス変数")
+print(F"プレイヤー1:{p1.score}点\n"
+      F"プレイヤー2:{p2.score}点")
+
+p1.score = 15   # これはインスタンス変数に代入していることになると推測
+p2.score = 20   # 〃
+
+print("値設定後、インスタンス変数、クラス変数")
+print(F"プレイヤー1:{p1.score}点\n"
+      F"プレイヤー2:{p2.score}点")
+print(F"プレイヤー1:{Player.score}点\n"
+      F"プレイヤー2:{Player.score}点")
+
+class Dog:
+    kind = "canine"
+
+    def __init__(self, name) -> None:
+        self.name = name
+
+d = Dog("Fido")
+e = Dog("Buddy")
+print(d.kind)
+print(e.kind)
+print(d.name)
+print(e.name)
+
+d.kind = "d の種類変更した"
+
+print(d.kind)
+print(e.kind)
+print(d.name)
+print(e.name)
