adf7724e6439c963876636ecbbe33380ef0ce3de
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-23 12:49:28 +0900

    🐜3 2 進数から 10 進数に変換する

--- comvert1.py
+++ comvert1.py
@@ -2,13 +2,11 @@
 伝統的なアルゴリズムで学ぶ定石と計算量
 増井 敏克 緒
 翔泳社 
-
-第2章 基本的なプログラムから作ってみる
-10 進数から 2 進数に変換する """
+"""
 
 n = 18
 
-def convert(n:int, base:int) -> str:
+def convert_from_decimal(n:int, base:int) -> str:
     """ 10 進数の数値 n を、base 進数の表記の文字列にして返します
 
     パラメーター
@@ -25,11 +23,28 @@ def convert(n:int, base:int) -> str:
 
     return result
 
-print(a := convert(n, 2))
-assert int(a, 2) == n
 
-print(a := convert(n, 3))
-assert int(a, 3) == n
+print(nstr := convert_from_decimal(n, 2))
+assert int(nstr, 2) == n
 
-print(a := convert(n, 8))
-assert int(a, 8) == n
+# 2 進数から 10 進数に変換する
+
+result = 0
+for i in range(len(nstr)):
+    result += int(nstr[i]) * (2 ** (len(nstr) - i - 1))
+
+print(result)
+assert result == n
+
+print()
+print("bin 関数で 10 進数から 2 進数に変換可能")
+print(bin(n))
+
+print()
+print("int 関数の第 2 引数に 2 を指定することで、2 進数の文字列から 10 進数に変換可能")
+print(int(nstr, 2))
+
+print()
+print("先頭に 0b とつけることで 2 進数のリテラルとして扱える")
+a = 0b10010
+print(a)
