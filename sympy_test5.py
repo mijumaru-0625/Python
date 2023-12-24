3200269aa76447b8c330a91f793fe0dde1d497f3
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-24 13:53:37 +0900

    🐜4 Best Proctices for eval() の手前まで

--- sympy_test5.py
+++ sympy_test5.py
@@ -29,8 +29,33 @@ print(f.subs(x, 1))
 print(f)
 print()
 
-class versin(Function):
-    pass
+from sympy import pi, Integer
 
+class versin(Function):
+    @classmethod
+    def eval(cls, x):
+        # if is an inter mulipleof pi, x/pi will cancel and be an Integer
+        n = x / pi
+        if isinstance(n, Integer):
+            return 1 - (-1) ** n
+        
+
+print("Create a Custom Function")
 print(versin(x))
 print(isinstance(versin(x), versin))
+print()
+
+print("Defining Automatic Evaluation with eval()")
+print(versin(pi))
+print(versin(2 * pi))
+print(versin(x * pi))
+print()
+
+class f(Function):
+    @classmethod
+    def eval(cls, x, y=1, *args):
+        return None
+
+print(f(1).args)
+print(f(1, 2).args)
+print(f(1, 2, 3).args)
