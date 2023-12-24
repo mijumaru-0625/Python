03695f5f3193941bd42044d94a3b0519bfd0256f
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-25 08:14:51 +0900

    🐜5 深さ優先探索

--- maze.py
+++ maze.py
@@ -22,23 +22,6 @@ https://qiita.com/sudo00/items/2b2eec07d3099b5ad664
 
 import time
 
-def output_test():
-    """ 標準出力への出力方法のテスト """
-    print("標準出力時に上書きしながら表示")
-    for i in range(10):
-        print("\r" + str(i), end="")
-        time.sleep(1)
-
-    print("\n")
-    print("複数行まとめて上書き")
-
-    for i in range(10):
-        print(str(i) + "\n" + str(i) + "\n" + str(i) + "\n" + "\033[3A", end="")
-        time.sleep(1)
-
-    print("\n\n\n")
-
-output_test()
 
 # 入力データ準備
 maze_str = """
@@ -76,12 +59,6 @@ for line in maze_str.split("\n"):
 
 import pprint
 
-print("入力データ maze の確認")
-pprint.pprint(maze)
-print()
-
-print("入力データを図で確認")
-
 def print_maze():
     for line in maze:
         for c in line:
@@ -99,37 +76,38 @@ def print_maze():
 print_maze()
 
 print()
-print("迷路の探索（番兵）幅優先探索")
-# スタート位置（x 座標、y 座標、移動回数）をセット
-pos = [[1, 1, 0]]
+print("迷路の探索（番兵）深さ優先探索")
 
-while len(pos) > 0:
-    x, y, depth = pos.pop(0) # リストから探索する位置を取得
-
-    # ゴールにつくと終了
+def search(x, y, depth):
+    """ 深さ優先探索 """
+    # ゴールに着くと終了
     if maze[y][x] == 1:
         print(depth)
-        print("\n" * len(maze))
-        break
-
+        print_maze()
+        exit() # 再帰呼び出しとなっているので　return ではだめ
+    
     # 探索済みとしてセット
     maze[y][x] = 2
 
-    # 上下左右を探索
-    if maze[y - 1][x] < 2:
-        pos.append([x, y - 1, depth + 1]) # 異動回数を増やして上をリストに追加
-    
-    if maze[y + 1][x] < 2:
-        pos.append([x, y + 1, depth + 1]) # 異動回数を増やして下をリストに追加
-    
-    if maze[y][x - 1] < 2:
-        pos.append([x - 1, y, depth + 1]) # 異動回数を増やして左をリストに追加
-    
-    if maze[y][x + 1] < 2:
-        pos.append([x + 1, y, depth + 1]) # 異動回数を増やして左をリストに追加
-
-
-    print(depth)
+    print(str(depth) + " ")
     print_maze()
     time.sleep(0.2)
     print(F"\033[{len(maze) + 2}A")
+
+    # 上下左右を探索
+    if maze[y - 1][x] < 2:
+        search(x, y - 1, depth + 1)     # 移動回数を増やして上を探索
+
+    if maze[y + 1][x] < 2:
+        search(x, y + 1, depth + 1)     # 移動回数を増やして下を探索
+        
+    if maze[y][x - 1] < 2:
+        search(x - 1, y, depth + 1)     # 移動回数を増やして左を探索
+
+    if maze[y][x + 1] < 2:
+        search(x + 1, y, depth + 1)     # 移動回数を増やして右を探索
+
+
+
+# スタート位置から開始
+search(1, 1, 0)
