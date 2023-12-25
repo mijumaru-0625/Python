e913785cc267f1d6f17578470b1082f0ed45d885
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-25 12:02:36 +0900

    🐜1 Tk のチュートリアルのサンプルプログラム
    https://tkdocs.com/tutorial/firstexample.html

---
+++ first_example.py
@@ -0,0 +1,42 @@
+"""
+https://tkdocs.com/tutorial/firstexample.html
+"""
+
+from tkinter import *
+from tkinter import ttk
+
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+    except ValueError:
+        pass
+
+root = Tk()
+root.title("メートルに換算")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="計算", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="フィート").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="は、次と同じです。").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="[m]").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+root.mainloop()
