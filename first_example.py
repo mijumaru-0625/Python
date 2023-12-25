4737852e33b86dea01e5c955d9854329e643b565
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-25 12:50:52 +0900

    🐜4 Widget の階層構造の表示
    https://tkdocs.com/tutorial/concepts.html

--- first_example.py
+++ first_example.py
@@ -1,46 +1,51 @@
 """
 https://tkdocs.com/tutorial/firstexample.html
 """
+
 from tkinter import *
 from tkinter import ttk
 
-class FeetToMeters:
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
 
-    def __init__(self, root):
-
-        root.title("Feet to Meters")
-
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-       
-        self.feet = StringVar()
-        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2, row=1, sticky=(W, E))
-        self.meters = StringVar()
-
-        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
-
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
-
-        for child in mainframe.winfo_children(): 
-            child.grid_configure(padx=5, pady=5)
-            print(str(child))
-
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
-        
-    def calculate(self, *args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-        except ValueError:
-            pass
+        print_hierarchy(root)
+    except ValueError:
+        pass
 
 root = Tk()
-FeetToMeters(root)
+root.title("メートルに換算")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+feet = StringVar()
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+meters = StringVar()
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+
+ttk.Button(mainframe, text="計算", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="フィート").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="は、次と同じです。").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="[m]").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind("<Return>", calculate)
+
+def print_hierarchy(w, depth=0):
+    print('  '*depth + w.winfo_class() + ' w=' + str(w.winfo_width()) + ' h=' + str(w.winfo_height()) + ' x=' + str(w.winfo_x()) + ' y=' + str(w.winfo_y()))
+    for i in w.winfo_children():
+        print_hierarchy(i, depth+1)
+
+print_hierarchy(root)
+
 root.mainloop()
