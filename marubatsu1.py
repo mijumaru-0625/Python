a8e470bcd37b245789b577dbd31afababab78df6
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-31 19:07:37 +0900

    🐜6 p.153 3 目並べ

---
+++ marubatsu1.py
@@ -0,0 +1,76 @@
+""" p.151 3 目並べ """
+
+import random
+
+
+def print_maru_batsu(p1, p2):
+    """ 訳が分からないので盤面を表示します """
+    board_str = """
+　：　：　
+－＋－＋－
+　：　：　
+－＋－＋－
+　：　：　"""
+    board_str_list = list(board_str)
+    board_str_index_list = [i for i, s in enumerate(board_str_list) if s == "\u3000"]
+    left_top = 0b100000000
+    board_str_index_dict = {left_top >> i: board_str_index_list[i] for i in range(9)}
+
+    if p1_is_maru:
+        mark_p1 = "〇" 
+        mark_p2 = "Ｘ"
+    else:
+        mark_p1 = "Ｘ"
+        mark_p2 = "〇" 
+
+    for k, v in board_str_index_dict.items():
+        if k & p1:
+            board_str_list[v] = mark_p1
+
+        if k & p2:
+            board_str_list[v] = mark_p2
+
+    print("".join(board_str_list))
+
+
+goal = [
+    0b111000000, 0b000111000, 0b000000111,
+    0b100100100, 0b010010010, 0b001001001,
+    0b100010001, 0b001010100
+]
+
+# 3 つ並んだか判定
+def check(player):
+    for mask in goal:
+        if player & mask == mask:
+            return True
+        
+    return False
+
+
+# 交互に置く
+def play(p1, p2):
+    if check(p2): # 3 つ並んでいたら出力して終了
+        print("勝負あり", [bin(p1), bin(p2)])
+        return
+    
+    board = p1 | p2
+    if board == 0b111111111: # すべて置いたら引き分けで終了
+        print("引き分け", [bin(p1), bin(p2)])
+        return
+
+    # 置ける場所を探す
+    w = [i for i in range(9) if (board & (1 << i)) == 0]
+    # ランダムに置いてみる
+    r = random.choice(w)
+    p1 = p1 | (1 << r)
+    print_maru_batsu(p1, p2)
+
+    global p1_is_maru
+    p1_is_maru = not p1_is_maru
+    play(p2, p1)  # 手番を入れ替えて次を探す
+
+
+p1_is_maru = True
+play(0, 0)
+
