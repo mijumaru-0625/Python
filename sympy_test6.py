d47c4bfa2f94c793c13c78c5d476fb9ce46ec14e
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2023-12-31 11:36:41 +0900

    🐜1 やっぱり諦めた

--- sympy_test6.py
+++ sympy_test6.py
@@ -6,7 +6,7 @@ Advanced Expression Manipulation のページを実際に
 """
 
 # Understanding Expression Trees
-from sympy import Symbol, Function
+from sympy import Symbol, Function, UnevaluatedExpr
 from sympy.parsing.sympy_parser import parse_expr, T
 
 x = Symbol("x")
@@ -14,35 +14,18 @@ y = Symbol("y")
 
 local_dict = {}
 local_dict["x"] = x
-local_dict["y"] = y
+local_dict["y"] = y    
+local_dict["nint"] = Function("nint")
+local_dict["round"] = Function("round")
 
-# 計算のみ実行する自作関数
-def func_abc(x):
-    return abs(x) + 1
-
-
-class func_def(Function):
-    @classmethod
-    def eval(cls, x):
-        pass
-
-    # def _eval_evalf(self, prec):
-    #     return (abs(x) - 1)._eval_evalf(prec)
-    
-
-local_dict["func_abc"] = globals()["func_abc"]
-local_dict["func_def"] = globals()["func_def"]
-
-# Walking the Tree
-def pre(expr):
-    print(expr)
-    for arg in expr.args:
-        pre(arg)
 
 # Sympy を利用して式を評価すると簡単な関数は数値に変換してしまうことが判明した。
 # 元の数式が消えてしまう
-expr = parse_expr("x*y + 1 + abs(-3) + func_abc(-5.5) + func_def(4.5)", local_dict, T[2:5] , evaluate=False)
-print(F"expr = {expr}")
-#pre(expr)
+expr_str = "x*y + 1 + 2 + 3 + nint(5.5) + round(5.5, 2)"
+expr = parse_expr(expr_str, local_dict, T[2:5] , evaluate=False)
+expr_str2 = str(expr)
 
-print("x = 1、y = 1 を与えて計算", expr.evalf(subs={x: 1, y: 1}))
+
+print(F"expr = {expr}")
+print(F"expr_str2 = {expr_str2}")
+print("式に変換後は順番が変わる。どうしようもないのか?")
