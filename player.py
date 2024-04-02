9ded906ca434b2c28e55a42a5ba8f8ca4c5c03f4
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2024-04-02 15:44:22 +0900

    🐜8 ～p.104

--- player.py
+++ player.py
@@ -3,7 +3,14 @@ import pygame as pg
 class Player:
     """ 主人公 """
     def __init__(self) -> None:
-        self._image = pg.image.load("images/kaeru1.png")
+        self._images = [
+            pg.image.load("images/kaeru1.png"),
+            pg.image.load("images/kaeru2.png"),
+            pg.image.load("images/kaeru3.png"),
+            pg.image.load("images/kaeru4.png")
+        ]
+        self._cnt = 0
+        self._image = self._images[0]
         self._rect = pg.Rect(250, 550, 50, 50)
         self._speed = 10
 
@@ -18,6 +25,8 @@ class Player:
         if self._rect.x + vx < 0 or self._rect.x + vx > 550:
             vx = 0
         self._rect.x += vx
+        self._cnt += 1
+        self._image = self._images[self._cnt // 7 % 4]
 
     def draw(self, screen):
         """ 描画処理 """
