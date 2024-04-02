0af0eaea7d7a27e200b179555854c1131f1d27e0
mijumaru-0625 <pxkyh425@yahoo.co.jp>
2024-04-02 16:01:29 +0900

    🐜9 ～p.112

---
+++ enemy.py
@@ -0,0 +1,24 @@
+import pygame as pg
+import random
+
+class Enemy:
+    """ 敵 """
+    def __init__(self) -> None:
+        x = random.randint(100, 500)
+        y = random.randint(100, 200)
+        # プロパティ
+        self._image = pg.image.load("images/enemy1.png")
+        self._rect = pg.Rect(x, y, 50, 50)
+        self._vx = random.uniform(-4, 4)
+        self._vy = random.uniform(-1, -4)
+
+    def update(self):
+        if self._rect.x < 0 or self._rect.x > 500:
+            self._vx = -self._vx
+        if self._rect.y < 0:
+            self._vy = -self._vy
+        self._rect.x += self._vx
+        self._rect.y += self._vy
+
+    def draw(self, screen):
+        screen.blit(self._image, self._rect)
--- mygame.py
+++ mygame.py
@@ -1,11 +1,14 @@
 # 1.準備
 import pygame as pg, sys
-import player
+import player, enemy
 
 pg.init()
 screen = pg.display.set_mode((600, 650))
 pg.display.set_caption("MYGAME")
 player = player.Player()
+enemies: list[enemy.Enemy] = []
+for i in range(8):
+    enemies.append(enemy.Enemy())
 
 # 2.メインループ
 while True:
@@ -13,8 +16,12 @@ while True:
     screen.fill(pg.Color("NAVY"))
     # 4.入力チェックや判断処理
     player.update()
+    for e in enemies:
+        e.update()
     # 5.描画処理
     player.draw(screen)
+    for e in enemies:
+        e.draw(screen)
     # 6.画面の表示
     pg.display.update()
     pg.time.Clock().tick(60)
